fc1667728c0342ebf6a9f0401305666a1a31565e
Vo Ho Thai Khang <25ad11001@hcmute.student.edu.vn>
2026-09-18 09:28:34 +0700

    bai 02

---
+++ practicestring2.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s1 = "Hello";
+    string s2 = "C++";
+    cout << s1 << endl;
+    cout << s2 << endl;
+
+    string name;
+    cout << "Enter your name: ";
+    getline(cin, name);
+    cout << "Hello, " << name << "!" << endl;
+
+    string first = "Hello";
+    string second = "World";
+    string message = first + " " + second + "!";
+    cout << message << endl;
+
+    string s = "Hello C++";
+    cout << "Length: " << s.length() << endl;
+
+    s = "Hello";
+    cout << "First character: " << s[0] << endl;
+    cout << "Third character: " << s[2] << endl;
+
+    string text = "I love C++!";
+    size_t pos = text.find("C++");
+    if (pos != string::npos) {
+        cout << "Found at position: " << pos << endl;
+    } else {
+        cout << "Not found!" << endl;
+    }
+
+    text = "Hello C++ World";
+    string part = text.substr(6, 3);
+    cout << part << endl;
+
+    s1 = "apple";
+    s2 = "apple";
+    if (s1.compare(s2) == 0) {
+        cout << "The strings are equal." << endl;
+    } else {
+        cout << "The strings are different." << endl;
+    }
+
+    return 0;
+}
